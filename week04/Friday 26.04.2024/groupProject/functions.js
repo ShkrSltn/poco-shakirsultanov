72a228e502aacf7d11394cf850275dc2a092152b
ShkrSltn <shakir.sultanov@powercoders.org>
2024-04-26 13:50:12 +0200

    Script worked

---
+++ week04/Friday 26.04.2024/groupProject/functions.js
@@ -0,0 +1,50 @@
+function appendStudentsToUl(studentList, ulElement) {
+    studentList.forEach((student, index) => {
+        const li = document.createElement('li');
+        li.classList.add(`student-${index}`); // Добавляем класс "student-N", где N - индекс студента в массиве
+
+        const name = document.createElement('p');
+        name.textContent = `Name: ${student.name}`;
+        name.classList.add('student-name'); // Добавляем класс "student-name" к элементу <p> с именем
+        li.appendChild(name);
+
+        const skills = document.createElement('p');
+        skills.textContent = `Skills: ${student.skill.join(', ')}`;
+        skills.classList.add('student-skills'); // Добавляем класс "student-skills" к элементу <p> с навыками
+        li.appendChild(skills);
+
+        const hobbies = document.createElement('p');
+        hobbies.textContent = `Hobbies: ${student.hobby.join(', ')}`;
+        hobbies.classList.add('student-hobbies'); // Добавляем класс "student-hobbies" к элементу <p> с хобби
+        li.appendChild(hobbies);
+
+        const photo = document.createElement('img');
+        photo.src = student.photo;
+        photo.classList.add('student-photo'); // Добавляем класс "student-photo" к изображению
+        li.appendChild(photo);
+
+        ulElement.appendChild(li);
+    });
+}
+
+const studentList = [{
+    name: "Olha",
+    skill: ["HTML", "CSS", "JS"],
+    hobby: ["programming", "reading"],
+    photo: "img/Olha-1.jpg"
+},
+{
+    name: "Olha",
+    skill: ["HTML", "CSS", "JS"],
+    hobby: ["programming", "reading"],
+    photo: "img/Olha-1.jpg"
+},
+{
+    name: "Olha",
+    skill: ["HTML", "CSS", "JS"],
+    hobby: ["programming", "reading"],
+    photo: "img/Olha-1.jpg"
+}];
+
+const ul = document.getElementById('studentList');
+appendStudentsToUl(studentList, ul);
---
+++ week04/Friday 26.04.2024/groupProject/script.js
@@ -0,0 +1,8 @@
+const studentsList = [
+    { name: 'John', age: 20, grade: 'A' },
+    { name: 'Jane', age: 19, grade: 'B' },
+    { name: 'Mike', age: 21, grade: 'A-' },
+
+];
+
+console.log(studentsList);

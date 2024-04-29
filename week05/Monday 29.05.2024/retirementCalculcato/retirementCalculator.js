9542c8592e8ecee685974320cf5ee3d83ae4ebd4
ShkrSltn <shakir.sultanov@powercoders.org>
2024-04-29 13:01:16 +0200

    Exercises for 29.05.2024 Monday have done

--- week05/Monday 29.05.2024/retirementCalculcato/retirementCalculator.js
+++ week05/Monday 29.05.2024/retirementCalculcato/retirementCalculator.js
@@ -13,6 +13,7 @@ function retirementCalculator() {
         return;
     }
 
+
     let years_left = retirement_age - current_age;
     let current_year = new Date().getFullYear();
     let retirement_year = current_year + years_left;
@@ -33,7 +34,7 @@ function retirementCalculator() {
 
     } else {
         console.log("You have " + years_left + " years left until retirement.");
-        console.log("You can retire in " + retirement_year + ".");
+        console.log("You can retire in " + retirement_year);
     }
 
 }
